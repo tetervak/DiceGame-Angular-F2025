e266eaebafbfd2a6ff8a7e13e32f7b89a41c8bae
Alex Tetervak <tetervak@sheridancollege.ca>
2025-09-10 11:23:31 -0400

    made one dice work

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,4 +14,13 @@ import {MyFooter} from './my-footer/my-footer';
 })
 export class App {
 
+  diceValue: number = 1;
+
+  getRandonDiceValue(): number{
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  onRollDice(): void {
+    this.diceValue = this.getRandonDiceValue();
+  }
 }
